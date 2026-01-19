ac1c6acba99b223c4aa12ae9e03be7cd13c756d1
MeetUnadakat07 <meetunadakat06@gmail.com>
2026-01-19 19:24:10 +0530

    fix profilePage

--- Task_3/src/data/profileData.js
+++ Task_3/src/data/profileData.js
@@ -1,6 +1,6 @@
 export const profileData = {
-  username: "alice",
-  fullName: "Alice Johnson",
+  username: "meet",
+  fullName: "Meet",
   avatar:
     "https://i.pinimg.com/736x/cd/e7/73/cde773e113620c91851ff948f76ad7dd.jpg",
   bio: "Photographer | Traveler | Dreamer 🌍📸",
